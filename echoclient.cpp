da83881f21c51f41b28171f9b3bc68d684d7abbe
insaneMelodist <steve.correlemeur@gmail.com>
2017-05-16 11:10:49 +0200

    Update echoclient.cpp

--- echoclient.cpp
+++ echoclient.cpp
@@ -36,6 +36,58 @@ void EchoClient::onTextMessageReceived(QString message)
     traitement(message);
 }
 
+int EchoClient::decodeMTHO2(QString mtho2)
+{
+//-------------------------------variables
+  string globalstring = mtho2.toStdString();
+  stringstream sstmsb = globalstring.substr(0, 8);
+  stringstream sstlsb = globalstring.substr(8, 16);
+  string hexatmsb;
+  string hexatlsb;
+  string hexastr;
+  int temp;
+
+//---------------------------------conversion bin-string ==> hex-sstream
+  bitset<8> setM(tmsb);
+  sstmsb << hex << setM.to_ulong() << endl;
+
+  bitset<8> setL(tlsb);
+  sstlsb << hex << setL.to_ulong() << endl;
+
+
+//----------------------------------verification taille chaines(sachant que cette chaine est plus grande de 1 charactere)
+  if (sstmsb.str().length() == 2)
+  {
+    hexatmsb = '0' + sstmsb.str();
+  }
+  else
+  {
+    hexatmsb = sstmsb.str();
+  }
+
+  if (sstlsb.str().length() == 2)
+  {
+    hexatlsb = '0' + sstlsb.str();
+  }
+  else
+  {
+    hexatlsb = sstlsb.str();
+  }
+
+//----------------------------------concatenation (et suppression des characteres mysteres)
+hexatmsb = hexatmsb.substr(0, hexatmsb.length()-1);
+hexatlsb = hexatlsb.substr(0, hexatlsb.length()-1);
+hexastr = hexatmsb + hexatlsb;
+cout << hexastr << ' ' << hexastr.size() << endl;
+
+//-----------------------------------conversion hexa-str ==> decimal-int
+
+temp = strtol(hexastr.c_str(), 0, 16);
+//cout << temp << endl;
+//-----------------------------------------------------------------
+  return temp;
+}
+
 void EchoClient::traitement(QString message){
 	//qDebug()<<message;
 	QJsonDocument jsonResponse = QJsonDocument::fromJson(message.toUtf8());
