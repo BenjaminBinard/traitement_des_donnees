79f6c7e5384e874583380c9b90ea51725d448e39
insaneMelodist <steve.correlemeur@gmail.com>
2017-05-16 09:39:55 +0200

    Add files via upload

---
+++ conversion.h
@@ -0,0 +1,13 @@
+#ifndef DECODAGE_H
+#define DECODAGE_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <bitset>
+#include <cstdlib>
+using namespace std;
+
+int decodeMTH02(string tmsb, string tlsb);
+
+#endif
